8d19452b54a56ace83d5367c6eaa6a11606ef43a
Andrey Alhimchenkov <a.alkhycmhenkov@gmail.com>
2019-11-04 08:40:27 +0200

    view+ext

---
+++ SearchPhoto/View+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  Extension.swift
+//  Sample_TableView
+//
+//  Created by Esat Kemal Ekren on 5.04.2018.
+//  Copyright © 2018 Esat Kemal Ekren. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchor (top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,  paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat, enableInsets: Bool) {
+        let topInset = CGFloat(0);
+        let bottomInset = CGFloat(0);
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+    }
+    
+}
+
